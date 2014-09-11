13ea89095f4469ef5614a6271770897089d30739
Pablo Dias <pablo.dias@superlogica.com>
2014-09-11 10:10:49 -0300

    Calculadora 2.0
    Alterações no codigo

--- Calculadora4Funcoes/calculadora4Funcoes/main.c
+++ Calculadora4Funcoes/calculadora4Funcoes/main.c
@@ -10,6 +10,7 @@ int main()
     //Três variaveis float, para armazenar os numeros colocados pelo usuario e o resultado
     float numero1, numero2, resultado;
 
+
     //Apresentação visual para o usuario
     printf("============================\n");
     printf("========Bem-Vindos a========\n");
@@ -18,7 +19,42 @@ int main()
     printf("\n\n\n");
 
 
-    printf("Digite a operação desejada:");
+    //Recebendo a operacao do Usuario
+    printf("Digite a operação desejada, sendo;\n");
+    printf("1 - para Soma, 2 - para Subtração,\n");
+    printf("3 - para Multiplicação, 4 - para Divisão:\n");
+    operacao =  getchar(); //Essa função pega apenas um digito do usuario
+    printf("\n\n\n");
 
-    return 0;
+
+    //Recebendo os valores para a operação
+    printf("Digite o primeiro valor:\n");
+    scanf("%f",&numero1);
+
+    printf("Digite o primeiro valor:\n");
+    scanf("%f",&numero1);
+    printf("\n\n\n");
+
+
+    //Iniciando o switch para gerar a operação correta
+    switch (operacao){
+        case 1:
+            resultado = numero1 + numero2;
+            printf("%f + %f = %f",numero1,numero2,resultado);
+            break;
+        case 2:
+            resultado = numero1 - numero2;
+            printf("%f - %f = %f",numero1,numero2,resultado);
+            break;
+        case 3:
+            resultado = numero1 * numero2;
+            printf("%f x %f = %f",numero1,numero2,resultado);
+            break;
+        case 4:
+            resultado = numero1 / numero2;
+            printf("%f / %f = %f",numero1,numero2,resultado);
+            break;
+    }
+
+    printf("\n\n\n");
 }
