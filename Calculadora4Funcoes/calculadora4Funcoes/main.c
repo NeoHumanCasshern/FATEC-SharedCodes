445bbb92106c3d691f56a70046adb6c916fae56c
Pablo Dias <pablo.dias@superlogica.com>
2014-09-11 09:53:00 -0300

    Arquivos Calculadora
    O começo do projeto da calculadora

---
+++ Calculadora4Funcoes/calculadora4Funcoes/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Iniciando as variaveis
+    //Uma varaiavel int para ser armazenada a escolha do usuario
+    int operacao;
+
+    //Três variaveis float, para armazenar os numeros colocados pelo usuario e o resultado
+    float numero1, numero2, resultado;
+
+    //Apresentação visual para o usuario
+    printf("============================\n");
+    printf("========Bem-Vindos a========\n");
+    printf("==a Calculadora 4 Funcoes!==\n");
+    printf("============================\n");
+    printf("\n\n\n");
+
+
+    printf("Digite a operação desejada:");
+
+    return 0;
+}
