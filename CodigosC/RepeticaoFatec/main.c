a1a3de10dbb29b78123a593f11ec30bae7da9d2c
Pablo Dias <pablo.dias@superlogica.com>
2014-09-16 12:44:31 -0300

    Correção dos Comentarios

--- CodigosC/RepeticaoFatec/main.c
+++ CodigosC/RepeticaoFatec/main.c
@@ -16,7 +16,9 @@ int main()
      printf("Quantas repeticoes deseja fazer?");
      scanf("%d",&numero);
 
-    //Estrutura basica do while para 100 repetições
+    //Estrutura basica do while sendo:
+	//"i" a variavel controladora e,
+	//"numero" o numero de repeticoes, inserido pelo usuario
     while (i<numero){
 
         printf("FATEC - %d!\n",i+1);
